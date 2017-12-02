35a77490085c6682e3c643cd0a02557c10447ca3
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-02 13:23:29 +0300

    [pet-sitter] 01 - add TODOs to SimpleOperationService

--- 01-ps-start-practice/src/main/java/com/ps/services/impl/SimpleOperationsService.java
+++ 01-ps-start-practice/src/main/java/com/ps/services/impl/SimpleOperationsService.java
@@ -25,14 +25,20 @@ public class SimpleOperationsService implements OperationsService {
     @Override
     public Response createResponse(Long sitterId, Long requestId) {
         // get sitter
-        // TODO 1. retrieve sitter * request  (according to diagram 2.5)
+        User sitter = userRepo.findById(sitterId);
 
         //create a response
         Response response = new Response();
-        //TODO 2. populate & save the response object
+        response.setResponseStatus(ResponseStatus.PROPOSED);
+        response.setUser(sitter);
+        response.setDetails("Testing createResponse()");
+
+        Request request = requestRepo.findById(requestId);
+        request.addResponse(response);
+
+        responseRepo.save(response);
         return response;
     }
-
 
     @Override
     public void acceptResponse(Long requestId, Long responseId) {
