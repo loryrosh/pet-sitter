f15ae884872b9e3e4d6ba44f3db8764282a86990
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-13 12:29:30 +0300

    [pet-sitter] 08 - TODO 40-42

--- 08-ps-jpa-practice/src/main/java/com/ps/repos/impl/JpaUserRepo.java
+++ 08-ps-jpa-practice/src/main/java/com/ps/repos/impl/JpaUserRepo.java
@@ -2,9 +2,11 @@ package com.ps.repos.impl;
 
 import com.ps.ents.User;
 import com.ps.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Set;
 
@@ -19,10 +21,11 @@ public class JpaUserRepo implements UserRepo {
 
     private EntityManager entityManager;
 
-    //TODO 42. Annotate this method with the proper annotation to make the repository class pass tests in TestJpaUserRepo
+    @PersistenceContext
     void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+
     @Override
     public List<User> findAll() {
         return entityManager.createQuery("select u from User u").getResultList();
